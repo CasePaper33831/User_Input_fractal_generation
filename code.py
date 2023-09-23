1827653e17c47512dbde13c7b35bbe02eddec25e
CasePaper33831 <87284113+CasePaper33831@users.noreply.github.com>
2023-09-23 12:02:25 -0500

    Update code.py
    removed zoom/move ability, updated TODO

--- code.py
+++ code.py
@@ -1,19 +1,18 @@
 '''
 TODO: 
--remove zoom/load ability
--user Input for what equation to use on render instance
+user Input for what equation to use on render instance
 
-FINISHED:
+FINISH:
+remove zoom/load ability
 popup
 load fractal
-WSAD for navigation
-Q and E zoom function
+arrows for navigation
+zoom function
 '''
 import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
-
 
 x_min, x_max, y_min, y_max = -2.0, 1.0, -1.5, 1.5
 zoom_factor = 1.1
@@ -56,50 +55,6 @@ def create_mandelbrot(width, height, max_iter):
 
     popup.geometry(f"{width}x{height}")
 
-def move_left(event):
-    global x_min, x_max
-    x_min -= pan_step
-    x_max -= pan_step
-    recreate_mandelbrot()
-
-def move_right(event):
-    global x_min, x_max
-    x_min += pan_step
-    x_max += pan_step
-    recreate_mandelbrot()
-
-def move_up(event):
-    global y_min, y_max
-    y_min += pan_step
-    y_max += pan_step
-    recreate_mandelbrot()
-
-def move_down(event):
-    global y_min, y_max
-    y_min -= pan_step
-    y_max -= pan_step
-    recreate_mandelbrot()
-
-def zoom_in(event):
-    global x_min, x_max, y_min, y_max
-    x_range = x_max - x_min
-    y_range = y_max - y_min
-    x_min += x_range / 2 * (1 - 1 / zoom_factor)
-    x_max -= x_range / 2 * (1 - 1 / zoom_factor)
-    y_min += y_range / 2 * (1 - 1 / zoom_factor)
-    y_max -= y_range / 2 * (1 - 1 / zoom_factor)
-    recreate_mandelbrot()
-
-def zoom_out(event):
-    global x_min, x_max, y_min, y_max
-    x_range = x_max - x_min
-    y_range = y_max - y_min
-    x_min -= x_range / 2 * (zoom_factor - 1)
-    x_max += x_range / 2 * (zoom_factor - 1)
-    y_min -= y_range / 2 * (zoom_factor - 1)
-    y_max += y_range / 2 * (zoom_factor - 1)
-    recreate_mandelbrot()
-
 def recreate_mandelbrot():
     global canvas_widget
     if canvas_widget:
@@ -110,16 +65,7 @@ def create_popup():
     global popup
     popup = tk.Toplevel(root)
     popup.title("Mandelbrot Set")
-
     create_mandelbrot(1500, 1500, 256)
-
-    # Bind keys for movement and zoom
-    popup.bind("w", move_up)
-    popup.bind("s", move_down)
-    popup.bind("a", move_left)
-    popup.bind("d", move_right)
-    popup.bind("q", zoom_in)
-    popup.bind("e", zoom_out)
 
 root = tk.Tk()
 root.after(0, create_popup)
