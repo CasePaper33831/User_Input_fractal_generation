e234a10a68caeed1eadbdcd40b2c4988d20a7c7a
CasePaper33831 <87284113+CasePaper33831@users.noreply.github.com>
2023-09-28 17:55:24 -0500

    Update code.py
    code and TODO update

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
 '''
 TODO: 
-debugging and making code SO MUCH faster
-From dev: Am well aware that this code is beyond slow lol. If you wanna help, feel free
+debug, figure out how to make equation so fractal loads every time
 
 
 FINISH:
@@ -11,67 +10,42 @@ load fractal
 arrows for navigation, zoom function (NOW SINCE REMOVED)
 user input for fractal math equation
 '''
-import tkinter as tk
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
+import matplotlib.pyplot as plt
 
-x_min, x_max, y_min, y_max = -2.0, 1.0, -1.5, 1.5
-zoom_factor = 1.1
-pan_step = 0.1
+rows = 3000
+cols = 3000
+iterations = 150
 
 equation_fractal = input("Please input the equation you wish for the fractal: ")
-canvas_widget = None
 
-def mandelbrot(c, max_iter):
-    z = 0
-    n = 0
-    while abs(z) <= 2 and n < max_iter:
-       equation_fractal
-    n += 1
-    return n
+def mandelbrot(c, z):
+    global iterations
+    count = 0
+    for a in range(iterations):
+        equation_fractal
+        count += 1
+        if(abs(z) > 4):
+            break
+    return count
+print("Please wait for fractal render!")
 
-def create_mandelbrot(width, height, max_iter):
-    global canvas_widget
-    fig, ax = plt.subplots(figsize=(width / 100, height / 100), dpi=100)
-    x, y = np.linspace(x_min, x_max, width), np.linspace(y_min, y_max, height)
-    X, Y = np.meshgrid(x, y)
-    C = X + 1j * Y
-    Z = np.zeros_like(C, dtype=np.int32)
+def mandelbrot_set(x, y):
+    m = np.zeros((len(x), len(y)))
+    for i in range(len(x)):
+        for j in range(len(y)):
+            c = complex(x[i], y[j])
+            z = complex(0, 0)
+            count = mandelbrot(c, z)
+            m[i, j] = count
+    return m
 
-    for i in range(width):
-        for j in range(height):
-            Z[i, j] = mandelbrot(C[i, j], max_iter)
+x = np.linspace(-2, 1, rows)
+y = np.linspace(-1, 1, cols)
 
-    ax.imshow(Z.T, extent=(x_min, x_max, y_min, y_max), cmap='hot', origin='lower')
-    ax.set_title("Mandelbrot Set")
-    ax.axis('off')
+m = mandelbrot_set(x, y)
 
-    canvas = FigureCanvasTkAgg(fig, master=popup)
-    canvas_widget = canvas.get_tk_widget()
-    canvas_widget.pack(fill=tk.BOTH, expand=True)
-    canvas.draw()
-
-    popup.geometry(f"{width}x{height}")
-
-def recreate_mandelbrot():
-    global canvas_widget
-    if canvas_widget:
-        canvas_widget.destroy()
-    create_mandelbrot(1500, 1500, 256)
-
-def create_popup():
-    global popup
-    popup = tk.Toplevel(root)
-    popup.title("Mandelbrot Set")
-    create_mandelbrot(1500, 1500, 256)
-
-root = tk.Tk()
-root.after(0, create_popup)
-
-def close_main_window():
-    root.withdraw()
-
-root.after(500, close_main_window)
-
-root.mainloop()
+plt.imshow(m.T, cmap = "magma")
+plt.axis("off")
+plt.savefig('mandelbrot_set.png', dpi=300, bbox_inches='tight')
+plt.show()
